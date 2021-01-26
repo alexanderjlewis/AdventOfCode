79284fc2f4b01de368c689b4862541513cc62f54
alexanderjlewis <alex.lewis1991@gmail.com>
2021-01-26 23:22:26 +1100

    day 13

--- 2019/lib/intcode.py
+++ 2019/lib/intcode.py
@@ -6,6 +6,7 @@ class IntcodeComp:
         self.rb = 0
         self.out = 0
         self.input = in_val.copy()
+        self.waiting_for_input = False
     
     def position(self,ms):
         t = []
@@ -36,9 +37,15 @@ class IntcodeComp:
                 self.inst[t[2]] = v[0] * v[1]
                 self.i += 4
             elif opcode == '03':
-                self.inst[t[0]] = self.input[0]
-                self.input.pop(0)
-                self.i += 2
+                if len(self.input) > 0:
+                    self.waiting_for_input = False
+                    self.inst[t[0]] = self.input[0]
+                    self.input.pop(0)
+                    self.i += 2
+                else:
+                    self.waiting_for_input = True
+                    self.out = None
+                    break
             elif opcode == '04':
                 self.out = v[0]
                 self.i += 2
