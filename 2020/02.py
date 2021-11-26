a6330e34b0fbad55d8511a84913d294b5f5446a1
alexanderjlewis <alex.lewis1991@gmail.com>
2021-11-26 20:23:55 +1100

    wip

---
+++ 2020/02.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from time import time
+from itertools import combinations
+from math import prod
+
+t0 = time()
+
+################ Data Processing #################
+
+fin = (Path(__file__).parent / "in/01.in").open()
+entries = [int(x) for x in fin.readlines()]
+
+target = 2020
+
+################ Common Function #################
+
+def func(target,items):
+    for x in combinations(entries, items):
+        if sum(x) == target:
+            return prod(x)
+
+################ Part 1 #################
+
+print('ans1:',func(target,2))
+
+################ Part 2 #################
+
+print('ans2:',func(target,3))
+
+################ Timing #################
+
+print("Time taken: %dms" % (1000 * (time() - t0)))
