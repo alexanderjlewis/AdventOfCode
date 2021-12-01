f976b33e64a5e0c5256135f0e4191432e9886427
alexander.lewis <alex.lewis1991@gmail.com>
2021-12-01 17:24:50 +1100

    wip

---
+++ 2020/11.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+from time import time
+from copy import deepcopy
+
+t0 = time()
+
+################ Data Processing #################
+
+fin = (Path(__file__).parent / "in/11_test.in").open().read()
+numbers = fin.split('\n')
+numbers = [list(number) for number in numbers]
+
+################ Common Function #################
+
+cols = len(numbers[0])
+rows = len(numbers)
+print('rows:',rows,', cols:',cols)
+
+adjacency = [(i,j) for i in (-1,0,1) for j in (-1,0,1) if not (i == j == 0)] #the adjacency matrix
+print(adjacency)
+
+numbers_next = deepcopy(numbers)
+
+def print_array(array1, array2):
+    print('in:             next:')
+    for i,item in enumerate(array1):
+        str=''
+        print(str.join(array1[i]),'    ',str.join(array2[i]),'    ',str.join(occ[i]))
+    print('----')
+
+#while True:
+for j in range(1):
+    occ = [['-']*10]*10
+    for x in range(0,cols):
+        for y in range(0,rows):
+            print(x,y)
+            if numbers[y][x] == '.':
+                continue
+
+            occupied_seats = 0
+            
+            for dx, dy in adjacency:
+                if 0 <= (x + dx) < cols and 0 <= (y + dy) < rows: #check boundaries
+                    if numbers[y][x] == '#':
+                        occupied_seats += 1
+            occ[y][x] = str(occupied_seats)
+            print(occ)
+
+            #print(x,y,occupied_seats)
+            if numbers[y][x] == 'L' and occupied_seats == 0:
+                numbers_next[y][x] = '#'
+            elif numbers[y][x] == '#' and occupied_seats >= 4:
+                numbers_next[y][x] = 'L'
+            
+            #print(occupied_seats)
+    
+            print_array(numbers, numbers_next)
+
+    if numbers == numbers_next:
+        print('break')    
+        break
+    else:
+        numbers = deepcopy(numbers_next)
+
+
+    
+
+################ Part 1 #################
+
+#print('ans1:',number)
+
+################ Part 2 #################
+
+#print('ans2:',min(test_list) + max(test_list))
+
+################ Timing #################
+
+print("Time taken: %dms" % (1000 * (time() - t0)))
---
+++ 2021/01.py
@@ -0,0 +1,40 @@
+
+from math import comb
+from pathlib import Path
+from time import time
+
+t0 = time()
+
+################ Data Processing #################
+
+fin = (Path(__file__).parent / "in/01.in").open().read()
+measurements = fin.split('\n')
+measurements = [int(measurement) for  measurement in measurements]
+
+################ Common Function #################
+
+################ Part 1 #################
+
+counter_1 = 0
+
+for i, measurement in enumerate(measurements[:-1]):
+    if measurements[i] < measurements[i+1]:
+        counter_1 += 1
+
+print('ans1:',counter_1)
+
+################ Part 2 #################
+
+counter_2 = 0
+
+for i, measurement in enumerate(measurements[:-3]):
+    group1 = sum(measurements[i:i+3])
+    group2 = sum(measurements[i+1:i+4])
+    if group1 < group2:
+        counter_2 += 1
+
+print('ans2:',counter_2)
+
+################ Timing #################
+
+print("Time taken: %dms" % (1000 * (time() - t0)))
