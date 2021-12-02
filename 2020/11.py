de6a3e8d2bbe76abd89ba7c946ed49bdb04f2d25
alexander.lewis <alex.lewis1991@gmail.com>
2021-12-03 09:29:10 +1100

    wip

--- 2020/11.py
+++ 2020/11.py
@@ -6,7 +6,7 @@ t0 = time()
 
 ################ Data Processing #################
 
-fin = (Path(__file__).parent / "in/11_test.in").open().read()
+fin = (Path(__file__).parent / "in/11.in").open().read()
 numbers = fin.split('\n')
 numbers = [list(number) for number in numbers]
 
@@ -14,10 +14,8 @@ numbers = [list(number) for number in numbers]
 
 cols = len(numbers[0])
 rows = len(numbers)
-print('rows:',rows,', cols:',cols)
 
 adjacency = [(i,j) for i in (-1,0,1) for j in (-1,0,1) if not (i == j == 0)] #the adjacency matrix
-print(adjacency)
 
 numbers_next = deepcopy(numbers)
 
@@ -28,12 +26,11 @@ def print_array(array1, array2):
         print(str.join(array1[i]),'    ',str.join(array2[i]),'    ',str.join(occ[i]))
     print('----')
 
-#while True:
-for j in range(1):
-    occ = [['-']*10]*10
+while True:
+    #for j in range(2):
+    occ= [['-']*cols for _ in range(rows)]
     for x in range(0,cols):
         for y in range(0,rows):
-            print(x,y)
             if numbers[y][x] == '.':
                 continue
 
@@ -41,10 +38,9 @@ for j in range(1):
             
             for dx, dy in adjacency:
                 if 0 <= (x + dx) < cols and 0 <= (y + dy) < rows: #check boundaries
-                    if numbers[y][x] == '#':
+                    if numbers[y + dy][x + dx] == '#':
                         occupied_seats += 1
             occ[y][x] = str(occupied_seats)
-            print(occ)
 
             #print(x,y,occupied_seats)
             if numbers[y][x] == 'L' and occupied_seats == 0:
@@ -54,7 +50,7 @@ for j in range(1):
             
             #print(occupied_seats)
     
-            print_array(numbers, numbers_next)
+    #print_array(numbers, numbers_next)
 
     if numbers == numbers_next:
         print('break')    
@@ -62,12 +58,9 @@ for j in range(1):
     else:
         numbers = deepcopy(numbers_next)
 
-
-    
-
 ################ Part 1 #################
 
-#print('ans1:',number)
+print('ans1:',sum([number.count('#') for number in numbers]))
 
 ################ Part 2 #################
 
---
+++ 2021/02.py
@@ -0,0 +1,53 @@
+
+from math import comb
+from pathlib import Path
+from time import time
+
+t0 = time()
+
+################ Data Processing #################
+
+fin = (Path(__file__).parent / "in/02.in").open().read()
+commands = fin.split('\n')
+commands = [command.split(' ') for command in commands]
+commands = [(x, int(y)) for x, y in commands] #cast the types
+
+################ Common Function #################
+
+################ Part 1 #################
+
+h_pos = 0
+depth = 0
+
+for command in commands:
+    action, qty = command
+    if action == 'forward':
+        h_pos += qty
+    elif action == 'down':
+        depth += qty
+    elif action == 'up':
+        depth -= qty
+
+print('ans1:',h_pos * depth)
+
+################ Part 2 #################
+
+h_pos = 0
+depth = 0
+aim = 0
+
+for command in commands:
+    action, qty = command
+    if action == 'forward':
+        h_pos += qty
+        depth += aim * qty
+    elif action == 'down':
+        aim += qty
+    elif action == 'up':
+        aim -= qty
+
+print('ans2:',h_pos * depth)
+
+################ Timing #################
+
+print("Time taken: %dms" % (1000 * (time() - t0)))
