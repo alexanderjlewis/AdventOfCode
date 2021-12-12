c3b78177670b7ff29cc13d0fd2974068d1f40559
Alex <alex.lewis1991@gmail.com>
2021-12-12 19:38:20 +1100

    wip

--- 2021/03.py
+++ 2021/03.py
@@ -1,29 +1,87 @@
 
 from pathlib import Path
 from time import time
+from copy import deepcopy
 
 t0 = time()
 
 ################ Data Processing #################
 
-fin = (Path(__file__).parent / "in/03_test.in").open().read()
-commands = fin.split('\n')
-commands = [command.split(' ') for command in commands]
-commands = [(x, int(y)) for x, y in commands] #cast the types
+fin = (Path(__file__).parent / "in/03.in")
+with open(fin, "r") as f:
+    numbers = [list(line.replace('\n','')) for line in f]
+    numbers = [[int(digit) for digit in number] for number in numbers]
 
 ################ Common Function #################
 
+#print(numbers)
 
+numbers_length = len(numbers)
 
 ################ Part 1 #################
 
+bits = zip(*numbers)
+bits = [sum(bit) for bit in bits]
 
-#print('ans1:',x)
+gamma_rate = ''
+epsilon_rate = ''
+
+for bit in bits:
+    if bit > numbers_length/2:
+        gamma_rate += '1'
+        epsilon_rate += '0'
+    else:
+        gamma_rate += '0'
+        epsilon_rate += '1'
+
+gamma_rate = int(gamma_rate,2)
+epsilon_rate = int(epsilon_rate,2)
+
+print('ans1:',gamma_rate * epsilon_rate)
 
 ################ Part 2 #################
 
+o2_rate = ''
+oxygen_rate = ''
 
-#print('ans2:',y)
+number_o2_gen = deepcopy(numbers)
+number_oxy_rate = deepcopy(numbers)
+
+bit_pos = 0
+
+while len(number_o2_gen) > 1:
+
+    bits = zip(*number_o2_gen)
+    bits = [sum(bit) for bit in bits]
+
+    if bits[bit_pos] >= len(number_o2_gen)/2:
+        number_o2_gen[:] = (x for x in number_o2_gen if x[bit_pos] == 1)
+    else:
+        number_o2_gen[:] = (x for x in number_o2_gen if x[bit_pos] == 0)
+
+    bit_pos += 1
+
+bit_pos = 0
+
+while len(number_oxy_rate) > 1:
+
+    bits = zip(*number_oxy_rate)
+    bits = [sum(bit) for bit in bits]
+
+    if bits[bit_pos] >= len(number_oxy_rate)/2:
+        number_oxy_rate[:] = (x for x in number_oxy_rate if x[bit_pos] == 0)
+    else:
+        number_oxy_rate[:] = (x for x in number_oxy_rate if x[bit_pos] == 1)
+
+    bit_pos += 1
+
+o2_rate = ''.join(map(str,number_o2_gen[0]))
+oxygen_rate = ''.join(map(str,number_oxy_rate[0]))
+
+o2_rate = int(o2_rate,2)
+oxygen_rate = int(oxygen_rate,2)
+
+print('ans2:',o2_rate * oxygen_rate)
 
 ################ Timing #################
 
--- 2021/04.py
+++ 2021/04.py
@@ -1,15 +1,58 @@
 
 from pathlib import Path
 from time import time
+from copy import deepcopy
+from math import sqrt
 
 t0 = time()
 
 ################ Data Processing #################
 
-fin = (Path(__file__).parent / "in/04_test.in").open().read()
-commands = fin.split('\n')
-commands = [command.split(' ') for command in commands]
-commands = [(x, int(y)) for x, y in commands] #cast the types
+boards = []
+board = []
+
+def format_line(line):
+    line = line.strip().replace('  ',' ').split(' ')
+    line = [int(x) for x in line]
+    return(line)
+
+fin = (Path(__file__).parent / "in/04.in")
+with open(fin, "r") as f:
+    draw_order = f.readline().strip().split(',')
+    draw_order = [int(item) for item in draw_order]
+    raw_boards = f.read().split('\n\n')
+    raw_boards = [board.strip().replace('\n',' ').replace('  ',' ').split(' ') for board in raw_boards]   
+    #boards = [[int(number) for number in board] for board in boards]  
+
+#rint(draw_order)
+#print(raw_boards)
+
+board_size = int(sqrt(len(raw_boards[0])))
+number_boards = len(raw_boards)
+
+#print(board_size,number_boards)
+
+boards = []
+
+for raw_board in raw_boards:
+    board = []
+    for i in range(board_size):
+        temp_rows = []
+        temp_cols = []
+        temp_rows = raw_board[i*board_size:(i*board_size)+board_size]
+        temp_cols = raw_board[i::board_size]
+        string_cols = []
+        string_rows = []
+        for j in temp_cols:
+            string_cols.append(int(j))
+        board.append(string_cols)
+        for j in temp_rows:
+            string_rows.append(int(j))
+        board.append(string_rows)
+        
+    boards.append(board)
+
+#print('boards',boards)
 
 ################ Common Function #################
 
@@ -17,13 +60,65 @@ commands = [(x, int(y)) for x, y in commands] #cast the types
 
 ################ Part 1 #################
 
+boards_1 = deepcopy(boards)
 
-#print('ans1:',x)
+moves = 0
+finish = False
+best_board = 0
+best_moves = 10000000
+
+for x, board in enumerate(boards_1):
+    moves = 0
+    finish = False
+    for card in draw_order:
+        moves += 1
+        for i in range(len(board)):
+            board[i] = [num for num in board[i] if num != card]
+        for row in board:
+            if len(row) == 0:
+                finish = True
+                break
+        if finish:
+            if moves < best_moves:
+                best_moves = moves
+                best_board = x
+            break
+
+unmarked_sum = int(sum([sum(x) for x in boards_1[best_board]]) / 2)
+last_ball = draw_order[best_moves - 1]
+
+print('ans1:',unmarked_sum * last_ball)
 
 ################ Part 2 #################
 
+boards_2 = deepcopy(boards)
 
-#print('ans2:',y)
+moves = 0
+finish = False
+best_board = 0
+best_moves = 0
+
+for x, board in enumerate(boards_2):
+    moves = 0
+    finish = False
+    for card in draw_order:
+        moves += 1
+        for i in range(len(board)):
+            board[i] = [num for num in board[i] if num != card]
+        for row in board:
+            if len(row) == 0:
+                finish = True
+                break
+        if finish:
+            if moves > best_moves:
+                best_moves = moves
+                best_board = x
+            break
+
+unmarked_sum = int(sum([sum(x) for x in boards_2[best_board]]) / 2)
+last_ball = draw_order[best_moves - 1]
+
+print('ans2:',unmarked_sum * last_ball)
 
 ################ Timing #################
 
