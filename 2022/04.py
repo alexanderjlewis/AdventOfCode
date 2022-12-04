c5cba08c8836ae155bb5f5f82914ce3489f53a8f
alexanderjlewis <alex.lewis1991@gmail.com>
2022-12-04 21:49:37 +1100

    wip

--- 2022/04.py
+++ 2022/04.py
@@ -5,53 +5,39 @@ t0 = time()
 
 ################ Data Processing #################
 
-fin = (Path(__file__).parent / "in/test/04.in") #(ANS1=2,ANS2=4)
+#fin = (Path(__file__).parent / "in/test/04.in") #(ANS1=2,ANS2=4)
 fin = (Path(__file__).parent / "in/04.in")
 
 sections = []
 with open(fin, "r") as f:
     data = f.read()   
     for line in data.split("\n"):
-        a,b = line.split(",")
-        al,au = a.split("-")
-        bl,bu = b.split("-")
-        sections.append([(int(al),int(au)),(int(bl),int(bu))]) 
+        sections.append(list(map(int,line.replace('-',',').split(','))))
 
 ################ Common Function #################
 
-
-
 ################ Part 1 #################
 
-overlaps = 0
+overlaps_1 = 0
+overlaps_2 = 0
 
 for row in sections:
-    a,b = row
-    al,au = a
-    bl,bu = b
+    s1, e1, s2, e2 = row
 
-    if bl>=al and bu<=au:
-        overlaps += 1
-    elif al>=bl and au<=bu:
-       overlaps += 1
+    r1 = set(range(s1,e1+1))
+    r2 = set(range(s2,e2+1))
 
-print('ans1:',overlaps)
+    if r1.issubset(r2) or r2.issubset(r1):
+        overlaps_1 += 1
+
+    if r1.intersection(r2):
+        overlaps_2 += 1
+
+print('ans1:',overlaps_1)
 
 ################ Part 2 #################
 
-overlaps = 0
-
-for row in sections:
-    a,b = row
-    al,au = a
-    bl,bu = b
-
-    if bl>au or al>bu:
-        pass
-    else:
-        overlaps += 1
-
-print('ans2:',overlaps)
+print('ans2:',overlaps_2)
 
 ################ Timing #################
 
