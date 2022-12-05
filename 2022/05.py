0f7e6baf2f71bde1bcb59ba0d44e739dbe6a367e
alexander.lewis <alex.lewis1991@gmail.com>
2022-12-05 21:22:20 +1100

    Day 5

--- 2022/05.py
+++ 2022/05.py
@@ -5,21 +5,29 @@ t0 = time()
 
 ################ Data Processing #################
 
-fin = (Path(__file__).parent / "in/test/05.in") #(ANS1=,ANS2=)
+fin = (Path(__file__).parent / "in/test/05.in") #(ANS1="CMZ",ANS2=)
 #fin = (Path(__file__).parent / "in/05.in")
 
-values = []
+procedures = []
+stacks = []
 with open(fin, "r") as f:
-    data = f.read()   
-    
+    data = f.read()
+    drawing, procedure_data = data.split("\n\n")   
 
+    procedure_data = procedure_data.replace('move ',"").replace(' to ',",").replace(' from ',",")
+    procedures = [list(map(int,a.split(','))) for a in procedure_data.split("\n")]
+
+    drawing = drawing.replace('   ','#').replace('[',"").replace(']',",").replace(' ',",")
+
+  
 ################ Common Function #################
 
 
 
 ################ Part 1 #################
 
-
+print(procedures)
+print(drawing)
 
 print('ans1:',)
 
