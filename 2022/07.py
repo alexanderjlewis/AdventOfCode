8488893c030661e1ccfedabf207677236375e6ed
alexanderjlewis <alex.lewis1991@gmail.com>
2022-12-07 22:37:28 +1100

    Day 7 complete

--- 2022/07.py
+++ 2022/07.py
@@ -5,83 +5,74 @@ t0 = time()
 
 ################ Data Processing #################
 
-fin = (Path(__file__).parent / "in/test/07.in") #(ANS1=95437,ANS2=)
-#fin = (Path(__file__).parent / "in/07.in")
+#fin = (Path(__file__).parent / "in/test/07.in") #(ANS1=95437,ANS2=24933642)
+fin = (Path(__file__).parent / "in/07.in")
 
 commands = []
 with open(fin, "r") as f:
     datastream = f.read()
     commands = [a.split(" ") for a in datastream.split("\n")]
 
-print(commands)
+################ Common Function #################
 
-commands.pop(0) #remove the "$ cd /" at start of file
-
-dirs_under_10k = {}
-
-def calc_dir_size(dir):
+def calc_dir_size():
+    
     value = 0
 
     while True:
-        if commands[0][0] == "$": #it's a commmand
-            if commands[0][4] == "$ cd ..":
-                commands.pop(0)
+        current_command = commands.pop(0)
 
-                if value <= 10000:
-                    dirs_under_10k[dir] = value
-
+        if current_command[0] == "$": #it's a commmand
+            if current_command[1] == "cd" and current_command[2] == "..":
+                
+                dirs.append(value)
                 break
-            elif commands[0][4] == "$ ls":
-                commands.pop(0)
-            else:
-                _,_,next_dir = commands[0].split(" ")
-                value
+
+            elif current_command[1] == "cd": #this mean we are changing to a new directory
+                value += calc_dir_size()
+            
+            #note we can ignore an LS command - doesn't do anything on its own
+    
         else: #it's a file or dir
 
+            if current_command[0] != "dir": #it's a file, so we add it's size on
+                value += int(current_command[0])
 
-    
-    commands.pop(0)
-    
-    return
+        if len(commands) == 0:
+            dirs.append(value)
+            break
 
+    return value
 
-
-while len(commands) > 0:
-    o = []
-
-    current_command = commands[0]
-
-    if current_command[0] == "$":
-        if current_command[1] == "ls":
-            commands.pop(0)
-            while True:
-
-        else: #must be a cd
-            if current_command[2] == "..": #move up a dir
-
-            else: #must be change to a lower dir
-
-    else: #line is an output
-        if current_command[0] == "dir":
-
-        else: #it's a file
-        
-
-
-################ Common Function #################
-
-def process_dir(dir_contents):
-    pass
 
 ################ Part 1 #################
 
-value = 0
+commands.pop(0) #remove the "$ cd /" at start of file
+dirs = []
+_ = calc_dir_size()
 
-print('ans1:',value)
+sum_over100k = 0
+for dir in dirs:
+    if dir <= 100000:
+        sum_over100k += dir
+
+print('ans1:',sum_over100k)
 
 ################ Part 2 #################
 
-print('ans2:',)
+free_space = 70000000 - max(dirs)
+extra_space_required = 30000000 - free_space
+
+delta = 70000000
+size = 0
+
+for dir in dirs:
+    if dir >= extra_space_required:
+        if (dir-extra_space_required) < delta:
+            delta = (dir-extra_space_required)
+            size = dir
+
+print('ans2:',size)
 
 ################ Timing #################
 
