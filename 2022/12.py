413798871f56a76fe5e262d7ebdefef3d85364c9
alexanderjlewis <alex.lewis1991@gmail.com>
2022-12-16 07:28:32 +1100

    Day 13 WIP

--- 2022/12.py
+++ 2022/12.py
@@ -1,33 +1,118 @@
 from pathlib import Path
 from time import time
+from copy import deepcopy
 
 t0 = time()
 
 ################ Data Processing #################
 
-fin = (Path(__file__).parent / "in/test/12.in") #(ANS1=,ANS2=)
-#fin = (Path(__file__).parent / "in/12.in")
+#fin = (Path(__file__).parent / "in/test/12.in") #(ANS1=31,ANS2=29)
+fin = (Path(__file__).parent / "in/12.in")
 
-values = []
+grid = []
 with open(fin, "r") as f:
-    data = f.read()   
-    
+    data = f.read().split("\n")
+    grid = [list(map(ord,a)) for a in data]
+   
+width = len(grid[0])
+height = len(grid)
+
+for i in range(width):
+    for j in range(height):
+        if grid[j][i] == 83: #start
+            start = (i,j)
+            grid[j][i] = 1
+        elif grid[j][i] == 69: #end
+            end = (i,j)
+            grid[j][i] = 26
+        else:
+            grid[j][i] = grid[j][i] - 96
+
 
 ################ Common Function #################
 
-
+dirs = [(0,1),(1,0),(-1,0),(0,-1)]
 
 ################ Part 1 #################
 
+def move_1(next):
 
+    coord, cost = next
+    cx, cy = coord 
+    
+    cont = False
 
-print('ans1:',)
+    if costs[cy][cx] == '':
+        costs[cy][cx] = cost
+        cont = True
+    elif cost < costs[cy][cx]:
+        costs[cy][cx] = cost
+        cont = True
+
+    cost += 1
+
+    if cont:
+        
+        for dir in dirs:
+            dx, dy = dir
+            nx = cx + dx
+            ny = cy + dy
+            if nx in range(width) and ny in range(height):
+                if grid[ny][nx] <= (grid[cy][cx] + 1):
+                    queue.append(((nx,ny),cost))
+
+costs = [['' for i in range(width)] for j in range(height)]
+visited = [[False for i in range(width)] for j in range(height)]
+queue = [(start,0)]
+
+while len(queue) > 0:
+    move_1(queue.pop(0))
+
+print('ans1:',costs[end[1]][end[0]])
 
 ################ Part 2 #################
 
+def move_2(next):
 
+    coord, cost = next
+    cx, cy = coord 
+    
+    cont = False
 
-print('ans2:',)
+    if costs[cy][cx] == '':
+        costs[cy][cx] = cost
+        cont = True
+    elif cost < costs[cy][cx]:
+        costs[cy][cx] = cost
+        cont = True
+
+    cost += 1
+
+    if cont and grid[cy][cx] != 1:
+        
+        for dir in dirs:
+            dx, dy = dir
+            nx = cx + dx
+            ny = cy + dy
+            if nx in range(width) and ny in range(height):
+                if grid[ny][nx] >= (grid[cy][cx] - 1):
+                    queue.append(((nx,ny),cost))
+
+costs = [['' for i in range(width)] for j in range(height)]
+visited = [[False for i in range(width)] for j in range(height)]
+queue = [(end,0)]
+routes_to_a = []
+
+while len(queue) > 0:
+    move_2(queue.pop(0))
+
+for i in range(width):
+    for j in range(height):
+        if grid[j][i] == 1:
+            if costs[j][i]:
+                routes_to_a.append(costs[j][i])
+
+print('ans2:',min(routes_to_a))
 
 ################ Timing #################
 
--- 2022/13.py
+++ 2022/13.py
@@ -5,21 +5,78 @@ t0 = time()
 
 ################ Data Processing #################
 
-fin = (Path(__file__).parent / "in/test/13.in") #(ANS1=,ANS2=)
+fin = (Path(__file__).parent / "in/test/13.in") #(ANS1=12,ANS2=)
 #fin = (Path(__file__).parent / "in/13.in")
 
-values = []
+pairs = []
 with open(fin, "r") as f:
-    data = f.read()   
-    
+    data = f.read().split("\n\n")
+    for x in data:
+        a,b = x.split("\n")
+        a = eval(a)
+        b = eval(b)
+        pairs.append([a,b])
 
 ################ Common Function #################
 
+def compare_items(l,r):
+    if type(l) == int and type(r) == int:
+        if l == r:
+            left.pop(0)
+            right.pop(0)
+            print('l=r')
+        elif l < r:
+            correct_indexes.append(i+1)
+            return True
+        else:
+            return False
+    
+    elif type(l) == list and type(r) == list:
+        return False
 
+    elif type(l) == int:
+        l = [l]
+        return False
+
+    elif type(r) == int:
+        r = [r]
+        return False
 
 ################ Part 1 #################
 
+correct_indexes = []
 
+for i in range(len(pairs)):
+
+    left,right = pairs[i]
+
+    while True:     
+        
+        l = None
+        r = None
+        print(i)
+        
+        if len(left) > 0:
+            l = left[0]
+        else:
+            correct_indexes.append(i+1)
+            break
+
+        if len(right) > 0:
+            r = right[0]
+        else:
+            break
+  
+        result = compare_items(l,r)
+        
+
+        
+
+        print(l,r)
+    i += 1
+        
+
+print('corr:',correct_indexes)
 
 print('ans1:',)
 
