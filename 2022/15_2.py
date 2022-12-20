1d37155a358e32f5177547f9c237a6dbb312d76a
alexanderjlewis <alex.lewis1991@gmail.com>
2022-12-20 22:18:31 +1100

    Day 15 update

--- 2022/15_2.py
+++ 2022/15_2.py
@@ -7,10 +7,10 @@ t0 = time()
 ################ Data Processing #################
 
 fin = (Path(__file__).parent / "in/test/15.in") #(ANS1=26,ANS2=56000011)
-#fin = (Path(__file__).parent / "in/15.in")
+fin = (Path(__file__).parent / "in/15.in")
 
 sensors = []
-taken = []
+beacons = []
 
 with open(fin, "r") as f:
     data = f.read().replace("Sensor at x=","").replace(" y=","").replace(": closest beacon is at x=",",").replace(", y=","")
@@ -20,51 +20,29 @@ with open(fin, "r") as f:
         sx,sy,bx,by = map(int,row)
         d = abs(sx-bx) + abs(sy-by)
         sensors.append(tuple([sx,sy,d]))
-        taken.append(tuple([bx,by]))
-        taken.append(tuple([sx,sy]))
+        beacons.append(tuple([bx,by]))
 
 y_val = 10
 y_val = 2000000
 
 ################ Common Function #################
 
-def all_coords_in_dist(base,dist):
-    
-    x,y = base
-    dx = 0
-    dy = y - y_val
 
-    t1 = time()
-    for dx in range(-dist,dist+1):
-        if (abs(dx) + abs(dy)) <= dist:
-            taken.add(tuple([x+dx,y_val]))
-    print("Func: %dms" % (1000 * (time() - t1)))
-    return
 
 ################ Part 1 #################
 
-min_y = min(taken)[1]
-max_y = max(taken)[1]
+no_beacon = set()
 
-print(min_y,max_y)
-'''
-for sensor in sensors:
-    sx,sy,d = sensors
+for sx,sy,d in sensors:
+    if abs(y_val-sy) <= d:
+        for dx in range(-d+abs(y_val-sy),d+1-abs(y_val-sy)):
+            if (abs(dx) + abs(y_val-sy)) <= d:
+                no_beacon.add((sx+dx,y_val))
 
-    if man_dist > 0:
-        all_coords_in_dist([sx,sy],man_dist)
-
-count = 0
-
-for item in taken:
-    x,y = item
+count = len(no_beacon)
+for _,y in beacons:
     if y == y_val:
-        count += 1
-
-for item in beacons:
-    x,y = item
-    if y == y_val:
-        count -= 1'''
+        count -= 1
 
 print('ans1:',count)
 
